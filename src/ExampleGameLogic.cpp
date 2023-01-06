e86399d6fc2ff0177c5d864073b480ae1fae2b55
Nikita Grigorev <pettyaelse@gmail.com>
2023-01-07 01:49:37 +0700

    add shaders

--- src/ExampleGameLogic.cpp
+++ src/ExampleGameLogic.cpp
@@ -1,12 +1,121 @@
 #include "ExampleGameLogic.h"
 
-#include "Input.h"
 #include "Engine.h"
+#include "Input.h"
+
+#include <iostream>
 
 void ExampleGameLogic::init()
 {
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+	const float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char *const vertex_shader_source = R"(
+		#version 330 core
+		layout (location = 0) in vec3 aPos;
+
+		void main()
+		{
+			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		}
+		)";
+
+	const char *const fragment_shader_source = R"(
+		#version 330 core
+		out vec4 FragColor;
+
+		void main()
+		{
+		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+		}
+		)";
+
+	unsigned int vertex_shader = create_vertex_shader(vertex_shader_source);
+	unsigned int fragment_shader = create_fragment_shader(fragment_shader_source);
+	unsigned int shader_program = create_shader_program(vertex_shader, fragment_shader);
+
+	// delete shaders
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	// TODO move?
+	glUseProgram(shader_program);
 }
+
+unsigned int ExampleGameLogic::create_shader_program(unsigned int vertex_shader,
+	unsigned int fragment_shader) const
+{
+	unsigned int shader_program;
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+
+	check_shader_linking(shader_program);
+
+	return shader_program;
+}
+
+unsigned int ExampleGameLogic::create_vertex_shader(const char *const vertex_shader_source) const
+{
+	unsigned int vertex_shader;
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+	glCompileShader(vertex_shader);
+
+	check_shader_compilation(vertex_shader);
+
+	return vertex_shader;
+}
+
+unsigned int ExampleGameLogic::create_fragment_shader(
+	const char *const fragment_shader_source) const
+{
+	unsigned int fragment_shader;
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+	glCompileShader(fragment_shader);
+
+	check_shader_compilation(fragment_shader);
+
+	return fragment_shader;
+}
+
+void ExampleGameLogic::check_shader_compilation(unsigned int shader_id) const
+{
+	int success;
+	glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char info_log[512];
+		glGetShaderInfoLog(shader_id, 512, nullptr, info_log);
+		std::cout << "SHADER COMPILATION ERROR:\n" << info_log << std::endl;
+	}
+}
+
+void ExampleGameLogic::check_shader_linking(unsigned int program_id) const
+{
+	int success;
+	glGetProgramiv(program_id, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		char info_log[512];
+		glGetProgramInfoLog(program_id, 512, nullptr, info_log);
+		std::cout << "LINKING COMPILATION ERROR:\n" << info_log << std::endl;
+	}
+}
+
 
 void ExampleGameLogic::update()
 {
--- src/ExampleGameLogic.h
+++ src/ExampleGameLogic.h
@@ -9,4 +9,12 @@ public:
 	void update() override;
 	void render() override;
 	void shutdown() override;
+
+private:
+	void check_shader_compilation(unsigned int shader_id) const;
+	void check_shader_linking(unsigned int program_id) const;
+	unsigned int create_vertex_shader(const char *const vertex_shader_source) const;
+	unsigned int create_fragment_shader(const char *const fragment_shader_source) const;
+	unsigned int create_shader_program(unsigned int vertex_shader,
+		unsigned int fragment_shader) const;
 };
